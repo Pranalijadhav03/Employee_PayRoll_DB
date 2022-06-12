79d0e0f6dbaed3bb9e5385d571a23f5b0c2bd931
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-06-12 21:23:46 +0530

    Added: EmpPayRoll_Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,13 @@
 ﻿------------UC1----------------
 create database payroll_server2
 use payroll_server2
+
+
+------------UC2----------------
+create table employee_payroll
+(
+id int identity,
+name varchar(35),
+salary money,
+start_date date
+)
