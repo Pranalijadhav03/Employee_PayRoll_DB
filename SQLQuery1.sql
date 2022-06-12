94b349671d2ba258f465243997f55897d8431678
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-06-12 21:31:13 +0530

    Added: PayRoll_Data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,3 +11,12 @@ name varchar(35),
 salary money,
 start_date date
 )
+
+
+-------------UC3---------------
+insert into employee_payroll values
+('Pranali',12000.0,'2018-06-09'),
+('Dipti',30000.0,'2022-01-07'),
+('Nutan',33300.0,'2020-09-09'),
+('Rachit',45000.0,'2022-08-07'),
+('Jyoti',39000.0,'2021-09-01')
